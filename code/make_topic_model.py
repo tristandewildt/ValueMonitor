89bd0f3e7bdaaaba317c065ffe7c8e7d6ec400f1
Tristan de Wildt <titan946@gmail.com>
2022-07-05 14:59:32 +0200

    Add files via upload

--- code/make_topic_model.py
+++ code/make_topic_model.py
@@ -323,8 +323,10 @@ def print_sample_articles_topic(df_with_topics, dict_anchor_words, topics, selec
     
     for index, row in sampled_df.iterrows():
         print('\033[1m' + 'Article '+str(index) + '\033[0m')
-        print("Title: "+str(row['title']))
-        print("Dataset: "+str(row['dataset']))
+        if 'title' in sampled_df:
+            print("Title: "+str(row['title']))
+        if 'dataset' in sampled_df:
+            print("Dataset: "+str(row['dataset']))
         
         text_combined_tagged = row['text_tagged']
         text_combined_not_tagged = row['text']
@@ -345,7 +347,6 @@ def print_sample_articles_topic(df_with_topics, dict_anchor_words, topics, selec
                     finish = min(len(tokens), index+window+1)
                     lhs = " ".join( tokens[start:index] )
                     rhs = " ".join( tokens[index+1:finish] )
-                    #conc = "%s [%s] %s" % (lhs, tokens[index], rhs)
                     conc = "%s [%s] %s" % (" - "+str(lhs), '\033[1m' + str(red(tokens[index])) + '\033[0m', rhs)
                     print(conc)
                     print("")
